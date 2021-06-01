8878e1aec143f584a0481b519273a4f51e4aa1c2
Phil <phil.flrs@gmail.com>
2021-06-01 13:35:09 -0600

    3.4 function component created for the login view and connected to main view. Used propTypes and useState
    Movies array is now loaded with the json data from /movies
    MovieCard uses propTypes and shapes data for the movieCard
    Two new views added for users to register and one for login
    LoginView function component uses react hook useState for username and password
    Main-view uses the loginView handleSubmit with onLoggedIn as a prop

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,33 +1,36 @@
 import React from "react";
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+
 
 class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: "Inception",
-          Description: "desc1...",
-          ImagePath: "...",
-        },
-        {
-          _id: 2,
-          Title: "The Shawshank Redemption",
-          Description: "desc2...",
-          ImagePath: "...",
-        },
-        {
-          _id: 3,
-          Title: "Gladiator",
-          Description: "desc3...",
-          ImagePath: "...",
-        },
-      ],
+      movies: [],
       selectedMovie: null,
-    };
+      user: null,
+    }
+  }
+  
+  componentDidMount(){
+    axios.get('https://myflixxapp.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
   }
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -35,17 +38,22 @@ class MainView extends React.Component {
     });
   }
   render() {
-    const { movies, selectedMovie } = this.state;
-  
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-  
+    
+    const { movies, selectedMovie, user } = this.state;
+
+    
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    if (movies.length === 0) return <div className="main-view" />;
+
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
       </div>
     );
